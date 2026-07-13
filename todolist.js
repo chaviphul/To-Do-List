ecd63a564f2f4a396f2a9d676a714e05b0d7920b
Chavi <chaviphull@gmail.com>
2026-07-13 15:23:46 +0530

    added local storage support for todo app

--- todolist.js
+++ todolist.js
@@ -1,12 +1,3 @@
-let addtask = document.getElementById("addtask");
-let nexttask = document.getElementById("nexttask");
-let main = document.getElementById("main");
-let task = document.getElementsByClassName("task");
-let total = document.getElementById("total");
-let completed = document.getElementById("completed");
-let pending = document.getElementById("pending");
-let checkbox = document.getElementsByClassName("checkbox")
-
 function updateTotal() {
     total.innerHTML = task.length;
 }
@@ -22,21 +13,57 @@ function updateCompletedandPending() {
     pending.innerHTML = task.length - count;
 }
 
-updateTotal();
-updateCompletedandPending();
+function renderTasks(){
+    main.innerHTML="";
+    for(let i=0; i<tasks.length; i++){
+        main.innerHTML = main.innerHTML + `<div class="task">
+                <input type="checkbox" class="checkbox">
+                <p>${tasks[i]}</p>
+                <button type="button" class="deletebtn"><i class="fa-solid fa-trash-can"></i></button>
+            </div>`
+    }
+    updateTotal();
+    updateCompletedandPending();
+}
+
+function updateTasks(text){
+    for(let i=0; i<tasks.length; i++){
+        if(text == tasks[i]){
+            tasks.splice(i,1);
+        }
+    }
+}
+
+
+
+
+let addtask = document.getElementById("addtask");
+let nexttask = document.getElementById("nexttask");
+let main = document.getElementById("main");
+let task = document.getElementsByClassName("task");
+let total = document.getElementById("total");
+let completed = document.getElementById("completed");
+let pending = document.getElementById("pending");
+let checkbox = document.getElementsByClassName("checkbox");
+
+
+let tasks=[];
+tasks=JSON.parse(localStorage.getItem("tasks"));
+if(tasks==null){
+    tasks=[];
+}
+
+renderTasks();
+
 
 
 addtask.onclick = function () {
     if (nexttask.value.trim() != "") {
-        main.innerHTML = main.innerHTML + `<div class="task">
-                <input type="checkbox" class="checkbox">
-                <p>${nexttask.value}</p>
-                <button type="button" class="deletebtn"><i class="fa-solid fa-trash-can"></i></button>
-            </div>`
+        tasks.push(nexttask.value);
+        localStorage.setItem("tasks", JSON.stringify(tasks))
+        renderTasks();
         nexttask.value = "";
-        updateTotal();
-        updateCompletedandPending();
-        
+
     }
 }
 
@@ -46,9 +73,12 @@ addtask.onclick = function () {
 main.onclick = function (event) {
     let btn = event.target.closest(".deletebtn");
     if (btn) {
-        btn.closest(".task").remove();
-        updateTotal();
-        updateCompletedandPending();
+        let this_task = btn.closest(".task");
+        let paragraph = this_task.querySelector("p");
+        let text = paragraph.textContent;
+        updateTasks(text);
+        localStorage.setItem("tasks",JSON.stringify(tasks));
+        renderTasks();
     }
 
     if (event.target.classList.contains("checkbox")) {
@@ -56,11 +86,18 @@ main.onclick = function (event) {
     }
 }
 
-nexttask.onkeydown = function(event){
-    if(event.key == "Enter"){
+nexttask.onkeydown = function (event) {
+    if (event.key == "Enter") {
         addtask.onclick();
     }
 }
+
+
+
+
+
+
+
 
 
 
